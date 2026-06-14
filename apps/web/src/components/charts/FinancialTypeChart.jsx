e347ee410eef4fce30989d771f93cdf2370bbe0d
rezaputrap <rezaaapp@gmail.com>
2026-06-14 08:38:09 +0700

    fix: mask chart axis values in hide mode

--- apps/web/src/components/charts/FinancialTypeChart.jsx
+++ apps/web/src/components/charts/FinancialTypeChart.jsx
@@ -10,9 +10,9 @@ import {
 } from "recharts";
 
 import {
+  formatPrivateCompact,
   formatPrivateRupiah,
   maskChartRows,
-  maskNumber,
 } from "../../utils/privacy";
 import { chartTheme } from "../../theme/chartTheme";
 
@@ -86,9 +86,7 @@ const FinancialTypeChart = ({
               <YAxis
                 stroke={colors.tick}
                 tick={{ fill: colors.tick, fontSize: 12 }}
-                tickFormatter={(value) =>
-                  `${(maskNumber(value, privacyMode) / 1000000).toFixed(0)}jt`
-                }
+                tickFormatter={(value) => formatPrivateCompact(value, privacyMode)}
               />
 
               <Tooltip
